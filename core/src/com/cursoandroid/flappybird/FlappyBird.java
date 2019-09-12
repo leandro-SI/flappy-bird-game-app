b4cb668376f2921016ddffc2325c037e085094c7
Leandro-SI <llmanutencao@bol.com.br>
2019-09-12 19:55:07 -0300

    criando pontuação

--- core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -2,8 +2,10 @@ package com.cursoandroid.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Random;
@@ -17,11 +19,14 @@ public class FlappyBird extends ApplicationAdapter {
 	private Texture canoTopo;
 	private Texture canoBaixo;
 	private Random numeroRandomico;
+	private BitmapFont fonte;
 
 	// Atributos de COnfiguração
 	private int movimento = 0;
 	private int larguraDispositivo;
 	private int alturaDispositivo;
+	private int estadoJogo = 0; // Iniciado ou não iniciado
+    private int pontuacao = 0;
 
 	private float variacao = 0;
 	private float velocidadeQueda = 0;
@@ -30,12 +35,17 @@ public class FlappyBird extends ApplicationAdapter {
 	private float espacoEntreCanos;
 	private float deltaTime;
 	private float alturaEntreCanosRandomica;
+	private  boolean marcouPonto = false;
 
 	
 	@Override
 	public void create () {
 
 		numeroRandomico = new Random();
+
+		fonte = new BitmapFont();
+		fonte.setColor(Color.WHITE);
+		fonte.getData().setScale(6);
 
 		// instancia para manipular as imagens
 		batch = new SpriteBatch();
@@ -63,34 +73,59 @@ public class FlappyBird extends ApplicationAdapter {
 	public void render () {
 
 		deltaTime = Gdx.graphics.getDeltaTime();
-
-		//variacao += 0.1;
-		// Diminuir variacao dos movimentos
 		variacao += deltaTime * 3;
 
-		posicaoMovimentoCanoHorizontal -= deltaTime * 150;
-
-		velocidadeQueda++;
-		//velocidadeQueda += Gdx.graphics.getDeltaTime() * 5;
-
-		if(variacao > 2){
+		if (variacao > 2) {
 			variacao = 0;
 		}
 
+		if(estadoJogo == 0){ // Não inciiado
 
-		// ver se a tela foi tocada para movimentar o passaro
-		if(Gdx.input.justTouched()){
-            //Gdx.app.log("Toque", "Toque na tela");
-			velocidadeQueda = -15;
-        }
+			if(Gdx.input.justTouched()){
+				estadoJogo = 1;
+			}
+		}else {
 
-		if(posicaoInicialVertical > 0 || velocidadeQueda < 0){
-			posicaoInicialVertical -= velocidadeQueda;
-		}
 
-		if(posicaoMovimentoCanoHorizontal < -100){
-			posicaoMovimentoCanoHorizontal = larguraDispositivo;
-			alturaEntreCanosRandomica = numeroRandomico.nextInt(400) - 200;
+
+
+			//variacao += 0.1;
+			// Diminuir variacao dos movimentos
+
+
+			posicaoMovimentoCanoHorizontal -= deltaTime * 150;
+
+			velocidadeQueda++;
+			//velocidadeQueda += Gdx.graphics.getDeltaTime() * 5;
+
+			// ver se a tela foi tocada para movimentar o passaro
+			if (Gdx.input.justTouched()) {
+				//Gdx.app.log("Toque", "Toque na tela");
+				velocidadeQueda = -15;
+			}
+
+			if (posicaoInicialVertical > 0 || velocidadeQueda < 0) {
+				posicaoInicialVertical -= velocidadeQueda;
+			}
+
+			// Verifica se cano saiu da tela
+			if (posicaoMovimentoCanoHorizontal < -100) {
+				posicaoMovimentoCanoHorizontal = larguraDispositivo;
+				alturaEntreCanosRandomica = numeroRandomico.nextInt(400) - 200;
+				marcouPonto = false;
+			}
+
+			//Verifica pontuacao
+			if(posicaoMovimentoCanoHorizontal < 120){
+
+				if(marcouPonto ==  false){
+					pontuacao++;
+					marcouPonto = true;
+				}
+
+			}
+
+
 		}
 
 		// iniciar exibição das imagens
@@ -100,6 +135,7 @@ public class FlappyBird extends ApplicationAdapter {
 		batch.draw(canoTopo, posicaoMovimentoCanoHorizontal,alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaEntreCanosRandomica);
 		batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica);
 		batch.draw(passaros[(int)variacao],120,posicaoInicialVertical);
+		fonte.draw(batch, String.valueOf(pontuacao), larguraDispositivo / 2, alturaDispositivo - 50);
 		movimento++;
 
 		batch.end();
